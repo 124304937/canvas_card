7c00757d0920d2dd4879a9664ee399300f00bd10
伊丽莎不白 <124304937@qq.com>
2019-07-09 18:15:44 +0800

    build: 添加项目模版

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,20 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
+    output: {
+        filename: '[hash].min.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin({
+            // inject: 'head',
+            title: '测试页面',
+            template: path.resolve(__dirname, 'public/index.html'),
+        })
+    ],
     devServer: {
         host: '0.0.0.0',
         port: 8000,
